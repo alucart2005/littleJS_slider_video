d8161d20a385d3c3c2ce578c644aea7d0fce8aba
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-07-15 17:04:18 -0500

    feat: End of the project

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const slideShow = document.getElementById('slideShow');
-const slides = document.getElementsByTagName('slideShow');
+const slides = slideShow.getElementsByTagName('video');
+const slideShowText = document.getElementById('slideShowText');
+const slidesText = slideShowText.getElementsByTagName('div');
+
+let index = 0;
 
 function toggleMenu() {
   const menuIcon = document.querySelector('.menuIcon');
@@ -7,4 +11,24 @@ function toggleMenu() {
   menuIcon.classList.toggle('active');
   navbar.classList.toggle('active');
 }
+
+function nextSlide() {
+  slides[index].classList.remove('active');
+  slidesText[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+  slidesText[index].classList.add('active');
+  console.log(index + "Next");
+}
+function prevSlide() {
+  slides[index].classList.remove('active');
+  slidesText[index].classList.remove('active');
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add('active');
+  slidesText[index].classList.add('active');
+  console.log(index + "Prev");
+}
+
+
+
 
